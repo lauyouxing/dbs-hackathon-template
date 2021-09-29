8a00d5254e9081ded00d8596ae94da7fcd28a80e
lauyouxing <youxing.lau.2018@smu.edu.sg>
2021-09-29 15:29:20 +0800

    Nav-Bar

--- banking-app/src/App.js
+++ banking-app/src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+import DBS from './DBS.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Nav, Navbar, NavDropdown } from 'react-bootstrap'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar bg="dark" variant="dark"
+      sticky = "top">
+        <Navbar.Brand>
+          <img src = {DBS} width="60px" height="40px" />{' '}
+        </Navbar.Brand>
+
+        <Nav>
+          <NavDropdown title="Products">
+            <NavDropdown.Item href="products/insurance">Insurance</NavDropdown.Item>
+            <NavDropdown.Item href="products/creditcard">CreditCard</NavDropdown.Item>
+            <NavDropdown.Divider/>
+            <NavDropdown.Item href="products/promo">Promo</NavDropdown.Item>
+          </NavDropdown>
+        <Nav.Link href="blog">Blog</Nav.Link>
+        <Nav.Link href="about-us">About Us</Nav.Link>
+        <Nav.Link href="contact-us">Contact Us</Nav.Link>
+        </Nav>
+      </Navbar>
+      <div className="content">
+        This is a content.
+      </div>
     </div>
   );
 }
